1ac5272acde095b1c8d601e2d59a10a029b5d64c
Fractal_Games <antonov.martin@gmail.com>
2024-04-14 21:10:19 +0300

    fixed cors problem

--- functions/main.py
+++ functions/main.py
@@ -3,18 +3,22 @@
 # Deploy with `firebase deploy`
 import os
 
-from firebase_functions import https_fn
+from firebase_functions import https_fn, options
 from firebase_admin import initialize_app
 from solver.solve_game import execute
+from flask import json
 
 initialize_app()
 
 
 @https_fn.on_request()
-def solve(req: https_fn.Request) -> https_fn.Response:
+def solve(req: https_fn.Request):
     game_text = req.form.get('game_text')
     file = open(os.path.join(os.getcwd(),"solver/input/game_to_solve.txt"), "w+")
     file.write(str(game_text))
     file.close()
     result = execute()
-    return result
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+    }
+    return (json.dumps(result), 200, headers)
